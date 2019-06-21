0a667f3e96866513bbd2a004356d4f4c5e801c9b
Martin Šturcel <martin@sturcel.sk>
2019-06-21 08:48:00 +0200

    Create svk.php

---
+++ web/lang/svk.php
@@ -0,0 +1,8 @@
+<?php
+
+define("_LASTDATE", "Posledný čas");
+define("_STATION", "Stanica");
+define("_CALLSIGN", "Volacia značka");
+define("_TIME", "Čas");
+define("_LAT", "Zemepisná šírka");
+define("_LON", "Zemepisná dĺžka");
