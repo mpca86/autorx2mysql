5da0f1298a6a3e89b30b24c3f9d8a2c9dbf7c591
Martin Šturcel <martin@sturcel.sk>
2019-06-21 09:21:42 +0200

    Update eng.php

--- web/lang/eng.php
+++ web/lang/eng.php
@@ -1,5 +1,8 @@
 <?php
 
+define("_UPDATEIN", "Update in");
+define("_SECONDS", "Seconds");
+define("_LASTSONDE", "Last Sonde");
 define("_LASTDATE", "Last Date");
 define("_STATION", "Station");
 define("_CALLSIGN", "Callsign");
@@ -7,6 +10,7 @@ define("_TIME", "Time");
 define("_FREQUENCY", "Frequency");
 define("_LAT", "Lat");
 define("_LON", "Lon");
+define("_SATELITES", "Satelites");
 define("_ALTITUDE", "Altitude (m)");
 define("_DIRECTION", "Direction");
 define("_DISTANCE", "Distance (km)");
