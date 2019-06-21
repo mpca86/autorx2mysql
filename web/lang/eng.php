3925c0607070573a484a1587ba97db5c24335a4c
Martin Šturcel <martin@sturcel.sk>
2019-06-21 08:47:42 +0200

    Create eng.php

---
+++ web/lang/eng.php
@@ -0,0 +1,8 @@
+<?php
+
+define("_LASTDATE", "Last Date");
+define("_STATION", "Station");
+define("_CALLSIGN", "Callsign");
+define("_TIME", "Time");
+define("_LAT", "Lat");
+define("_LON", "Lon");
